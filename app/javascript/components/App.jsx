07f69de22d34d20e8493014969db879bfa22dc5a
RushikeshDhaygude <rushikeshdhaygude9414@gmail.com>
2025-03-26 16:06:59 +0530

    fix dashboard ui

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -3,8 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <h1>Welcome to the Tournament Management System</h1>
-      <p>Organize and manage tournaments with ease. Create, join, and track matches in real-time!</p>
+      <h1>React Integrated </h1>
     </div>
   );
 };
